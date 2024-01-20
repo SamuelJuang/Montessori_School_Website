070f533332b9c8d986d7eb7e64ece9ed9ec92ba5
SamuelJuang <samueljuang007@gmail.com>
2024-01-20 09:50:38 +0700

    test commit branch

---
+++ src/components/aboutHome.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import defaultImg from "../assets/test.png";
+import tempImg from "../assets/temp.jpg";
+
+const AboutHome = () => {
+    return (
+           <div className='gap-2'>
+                <div className='flex lg:flex-row sm:flex-col'>
+                    <div className='basis-8/12 ps-5 pt-9 ms-9 mt-9'>
+                    <div className='flex flex-col gap-2'>
+                        <p className='text-purple-500'><strong>Know More About Us</strong></p>
+                            <h1 className='text-4xl font-bold'>About Montessori School</h1>
+                            <p className='text-slate-400 m-0 p-0 mt-5'>
+                            One of the defining features of Montessori education is the use of specialised learning materials designed to encourage hands-on learning, free from adult direction. These materials are designed to stimulate children’s minds, encourage exploration, and inspire independent learning.
+                            </p>
+
+                            <p className='text-slate-400 m-0 p-0 mt-2'>
+                            Montessori materials generally have special qualities that allow children the opportunity for success in their investigations into abstract concepts such as shapes and sizes.
+                            </p>
+                        
+                            <p className='text-slate-400 m-0 mt-2'>
+                            Discover a world of learning at our Montessori school! We empower children through personalized education, hands-on experiences, and a nurturing environment. Our approach fosters independence, curiosity, and a lifelong love for learning. Join us in embracing each child's unique journey to success!
+                            </p>
+                    </div>
+
+                    </div>
+                    <div className='basis-4/12 lg:ps-5 lg:pt-5 lg:pe-9 lg:me-9'>
+                        <div className='lg:mb-9 lg:mt-5'>
+                            <img className="object-cover rounded-xl sm:w-10/12 sm:h-1/5 sm:mt-5 sm:mx-auto" src={tempImg} alt="test"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='flex lg:flex-row md:flex-col sm:flex-col'>
+                    <div className='basis-5/12 ps-5 pe-5 ms-9 mt-9'>
+                        <div className='mb-5 mt-9'>
+                            <img className="object-cover rounded-xl" src={defaultImg} alt="test"/>
+                        </div>  
+                    </div>
+                    <div className='basis-7/12 ps-5 pt-9 pe-9 me-9 mb-5'>
+                        <div className='flex flex-col gap-2 '>
+                            <p className='text-slate-400 m-0 p-0 mt-5'>
+                            One of the defining features of Montessori education is the use of specialised learning materials designed to encourage hands-on learning, free from adult direction. These materials are designed to stimulate children’s minds, encourage exploration, and inspire independent learning.
+                            </p>
+
+                            <p className='text-slate-400 m-0 p-0 mt-2'>
+                            Montessori materials generally have special qualities that allow children the opportunity for success in their investigations into abstract concepts such as shapes and sizes.
+                            </p>
+                        
+                            <p className='text-slate-400 m-0 mt-2'>
+                            Discover a world of learning at our Montessori school! We empower children through personalized education, hands-on experiences, and a nurturing environment. Our approach fosters independence, curiosity, and a lifelong love for learning. Join us in embracing each child's unique journey to success!
+                            </p>
+                    </div>
+                    </div>
+                </div>
+           </div>
+    );
+};
+
+export default AboutHome;
