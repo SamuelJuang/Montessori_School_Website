e90357ee899b1be885c4f9a091cd439aa1c7c57f
Nicholas Yang <210711153@students.uajy.ac.id>
2024-02-01 15:42:49 +0700

    step 1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import VideoBackgroundHome from "../components/videobackgroundhome";
 import AboutHome from "../components/aboutHome";
+import PeekGalery from "../components/PeekGalery";
+import Testimonials from "../components/Testimonials";
 import { ReactLenis, useLenis } from '@studio-freight/react-lenis';
 import { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
-import { useGSAP } from "@gsap/react"; 
+import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger)
 
 function HomePage() {
   const lenisRef = useRef();
-  
+
   useEffect(() => {
     function update(time) {
       lenisRef.current?.lenis?.raf(time * 1000)
     }
-  
+
     gsap.ticker.add(update)
-  
+
     return () => {
       gsap.ticker.remove(update)
     }
@@ -25,10 +27,12 @@ function HomePage() {
 
   return (
     <ReactLenis root>
-    
-     <div style={{overflowY: "hidden"}}>
-        <VideoBackgroundHome/>
-        <AboutHome/>
+
+      <div style={{ overflowY: "hidden" }}>
+        <VideoBackgroundHome />
+        <AboutHome />
+        <PeekGalery />
+        <Testimonials />
       </div>
 
     </ReactLenis>
