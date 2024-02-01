d5bce88da489ba6a7cd5df85a0c309712adf08bf
Nicholas Yang <210711153@students.uajy.ac.id>
2024-02-01 15:50:16 +0700

    add peek gallery and testimonial

--- src/components/PeekGalery.jsx
+++ src/components/PeekGalery.jsx
@@ -3,34 +3,55 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 export default function PeekGalery() {
     const galleries = [
-        "../assets/videos/C0036.MP4",
-        "../assets/videos/C0039.MP4",
-        "../assets/videos/C0063.MP4",
-        "../assets/videos/C0066.MP4",
-        "../assets/videos/C0071.MP4",
-        "../assets/videos/C0092.MP4"
+        "src/assets/videos/C0036.MP4",
+        "src/assets/videos/C0039.MP4",
+        "src/assets/videos/C0063.MP4",
+        "src/assets/videos/C0066.MP4",
+        "src/assets/videos/C0071.MP4",
+        "src/assets/videos/C0092.MP4"
     ];
 
     return (
         <>
-            <p className="text-5xl font-semibold text-center mt-10">Peek The Gallery</p>
-            <Swiper navigation={true} pagination={{ clickable: true }} modules={[Navigation, Pagination]} className="h-full w-3/4">
-                <ol>
-                    {galleries.map((content, index) => (
-                        <SwiperSlide key={index} className="h-full container mt-10">
-                            <div className="mx-40 bg-slate-100 rounded-3xl pt-5 pb-12">
-                                <div className="items-center pt-5 px-5">
-                                    <video src={content}></video>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </ol>
+            <p className="text-4xl font-semibold text-center mt-20">Peek The Gallery</p>
+            <Swiper
+                navigation={true}
+                rewind={true}
+                pagination={{ clickable: true }}
+                modules={[Navigation, Pagination, Autoplay]}
+                breakpoints={{
+                    1536: {
+                        spaceBetween: 0
+                    },
+                    1280: {
+                        spaceBetween: 40
+                    },
+                    1024: {
+                        spaceBetween: 60
+                    },
+                    768: {
+                        spaceBetween: 50
+                    },
+                }}
+                spaceBetween={20}
+                // style={{ overflow: "visible" }}
+                className="h-full w-10/12 lg:w-1/5` items-center"
+                autoplay={{ delay: 5000 }}
+                centeredSlides={true}
+            >
+                {galleries.map((content, index) => (
+                    <SwiperSlide key={index} className="h-full mt-10">
+                        <div className="lg:mx-20 rounded-3xl pb-12 items-center">
+                            <video className="object-cover w-full aspect-video object-center rounded-xl mx-auto" src={content} autoPlay muted loop />
+                        </div>
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </>
     );
