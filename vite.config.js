0dd9fc8e3f84f9359aaa204e899733714ccf4c60
SamuelJuang <samueljuang007@gmail.com>
2024-02-12 23:01:54 +0700

    coba ubah huruf kecil

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.MP4'],
+  assetsInclude: ['**/*.mp4'],
   plugins: [react(),
   ],
 })
